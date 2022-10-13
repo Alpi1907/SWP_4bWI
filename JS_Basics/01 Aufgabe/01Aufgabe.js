7da0c5952cabf0500d9582fae9a435f06cfd7b83
Alpi1907 <103244184+Alpi1907@users.noreply.github.com>
2022-10-13 11:14:14 +0200

    01Aufgabe finished

---
+++ JS_Basics/01 Aufgabe/01Aufgabe.js
@@ -0,0 +1,35 @@
+//Variablen
+
+let a = 7;
+let b = 8;
+let c = 10;
+
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+let Wert = a+b+c;
+console.log("a + b+ c = " + Wert);
+// (a + b) / c
+Wert = (a+b)/c;
+console.log("(a + b) / c = " + Wert);
+// c -a + b
+Wert = c-a+b;
+console.log("c - a + b = " + Wert);
+// a + b + c * 3
+Wert = a+b+c*3;
+console.log("a + b + c * 3 = " +Wert)
+// (a + b + c)/4
+Wert = (a+b+c)/4;
+console.log("(a +b +c) / 4 = " + Wert)
+
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+a = 3;
+// b = a + 3
+b = a+3;
+// c = c -3
+c = c-3;
+// gib die neuen Werte der Variablen aus
+console.log("a hat einen neuen Wert von: " + a);
+console.log("b hat einen neuen Wert von: " + b);
+console.log("c hat einen neuen Wert von: " + c);
