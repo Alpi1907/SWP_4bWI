5cd66398724a9660a1e63de3fbf1aa57429bb866
Alpi1907 <103244184+Alpi1907@users.noreply.github.com>
2022-10-13 10:31:34 +0200

    index.html und script.js mit Video finished

---
+++ JS_Basics/01 JS Variablen & Operationen/script.js
@@ -0,0 +1,21 @@
+console.log("Hello");
+console.log("Tolle Sache");
+
+//variablen
+
+let a = 7;
+let b = 8;
+let c = a + b;
+
+console.log("A hat den Wert " + a);
+
+//a = 10;
+console.log("A hat den Wert " + a);
+
+console.log("a + b = " + c);
+
+//Rechnungsarten
+// *, /, +, -
+
+c = b-a;
+console.log("b - a = " + c);
