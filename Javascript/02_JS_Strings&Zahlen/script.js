683036bd07aff360621b464042774a957d581563
Alpi1907 <103244184+Alpi1907@users.noreply.github.com>
2022-10-14 15:32:07 +0200

    02_JS_Strings&Zahlen Video finished

---
+++ Javascript/02_JS_Strings&Zahlen/script.js
@@ -0,0 +1,22 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+console.log(ab);
+console.log(ab);
+
+// +, -, *, /
+
+let c = 7.4;
+let d = 7.5;
+
+let result = c+d;
+console.log(result);
+
+//Texte Strings
+
+let name = "Alperen"; 
+console.log(name);  
+
+console.log("Das Ergebnis von " + a + " und " + b + " ist " + ab);
