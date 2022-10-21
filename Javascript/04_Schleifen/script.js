4755645422ec80131ba2076399622f7e735a41fb
Alpi1907 <103244184+Alpi1907@users.noreply.github.com>
2022-10-21 15:50:01 +0200

    04_Schleifen Videos finished

---
+++ Javascript/04_Schleifen/script.js
@@ -0,0 +1,15 @@
+console.log("test");
+ /*
+let a = 7;
+let b = 4.3;
+let name = "Alperen"; //Text
+
+console.log(name + " " + b + a);
+*/
+//Schleife
+
+for(let count = 0; count<10;count++){
+    console.log(count + " Alperen")
+}
+
+console.log("fertig");
