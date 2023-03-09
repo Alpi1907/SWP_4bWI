06aac7726c193d1ea8bd3fedbf66022b6b19fad3
Alpi1907 <103244184+Alpi1907@users.noreply.github.com>
2023-03-09 10:26:17 +0100

    03_Football_API finished

---
+++ Javascript_&_Ajax/03_Football_API/script.js
@@ -0,0 +1,19 @@
+const loadData = () =>{
+    fetch("https://api.openligadb.de/getbltable/bl1/2022").then((result)=>{
+        result.json().then((data)=>{
+            console.log(data);
+            fillTable(data);
+        });
+    });
+};
+
+const fillTable = (data)=>{    
+    let html = "";
+
+    data.forEach(elemt => {
+        html += "<div>" + element.points + " by Team " + element.teamName + "</div>";
+    });
+    document.getElementById("output").innerHTML = html;
+};
+
+loadData();
