52ace43d8aec3c7f65f2e93bf5c033c8c8d1a83c
Alpi1907 <103244184+Alpi1907@users.noreply.github.com>
2023-06-15 10:49:44 +0200

    Arrow Functions durchgegangen

--- Javascript Extended/10_JS_Arrow_Functions(M)/arrowFunction.js
+++ Javascript Extended/10_JS_Arrow_Functions(M)/arrowFunction.js
@@ -2,18 +2,22 @@ function sum (a, b) {
     return a + b;
 }
 
-let sum2 = (a, b) => {
-    return a + b;
-}
+let sum2 = (a, b) => a + b
 
 function isPositive(number) {
     return number >= 0;
 }
 
 function randomNumber() {
-    return ;AuthenticatorAssertionResponse.random;
+    return ;Math.random;
+}
+
+let randomNumber2 = () => {
+    return ;Math.random;
 }
 
 document.addEventListener('click', function() {
     console.log('click');
 })
+
+document.addEventListener('click', () => console.log('click'))
